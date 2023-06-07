c31e335c8bfe6dfbe1d88c019c884052bf3fe33a
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-06-07 13:54:58 -0500

    Add preapend() method

--- script.js
+++ script.js
@@ -6,24 +6,43 @@ class Node {
 }
 
 class LinkedList {
+  // by default the list will have a head property set to null
   constructor() {
     this.head = null;
   }
 
+  // adds a new node containing value to the end of the list
   append(value) {
+    // call the Node Class constructor with the passed value
     const newNode = new Node(value);
 
+    // if there are no items (head is null), set the head as the newNode
     if (!this.head) {
       this.head = newNode;
       return;
     }
 
+    // current initially is the head of the list
     let current = this.head;
+
+    // navigate until the end of the list and set current to the last node
     while (current.next) {
       current = current.next;
     }
 
+    // add the newNode at the end of the list
     current.next = newNode;
+  }
+
+  // adds a new node containing value to the start of the list
+  preapend(value) {
+    const newNode = new Node(value);
+
+    // the new node points to the current head of the list
+    newNode.next = this.head;
+
+    // the new node becomes the new head of the list
+    this.head = newNode;
   }
 }
 
@@ -45,5 +64,6 @@ myExamleList = {
 let myList = new LinkedList();
 myList.append("Node 1");
 myList.append("Node 2");
+myList.preapend("hi");
 
 console.log(myList);
