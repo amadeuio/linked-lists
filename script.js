1c9f36e8003cbed681060d9a22d13235a930a7ef
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-06-07 13:28:27 -0500

    Add append() method

--- script.js
+++ script.js
@@ -9,9 +9,41 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
 }
+
+// to visualize what the linked list looks like
+myExamleList = {
+  head: {
+    data: "Node 1",
+    next: {
+      data: "Node 2",
+      next: {
+        data: "Node 3",
+        next: null,
+      },
+    },
+  },
+};
 
 // create a new LinkedList and set the head to our new Node
 let myList = new LinkedList();
+myList.append("Node 1");
+myList.append("Node 2");
 
 console.log(myList);
